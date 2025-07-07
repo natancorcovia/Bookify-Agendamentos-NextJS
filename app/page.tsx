3c63b47d73c5ee294a240b44efbe35775ca8ffd5
Natan Corcovia <natankorkov@gmail.com>
2025-07-07 04:06:23 -0300

    feat: add header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
+import { Input } from "./_components/ui/input"
+import Header from "./_components/Header"
+import { SearchIcon } from "lucide-react"
 import { Button } from "./_components/ui/button"
+import Image from "next/image"
 
 export default function Home() {
   return (
     <div>
-      <Button> Clique aqui </Button>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Natan!</h2>
+        <p>Segunda-feira, 07 de julho.</p>
+
+        <div className="mt-6 flex items-center gap-1">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende com os melhores!"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
     </div>
   )
 }
