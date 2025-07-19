8e4e02a7a98ab03b759c623aeab86139c4717eae
Natan Corcovia <natankorkov@gmail.com>
2025-07-19 15:15:20 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/Search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 export default async function Home() {
   const session = await getServerSession(authOptions)
@@ -46,8 +48,18 @@ export default async function Home() {
       <Header />
       <div className="p-5">
         {/* texto */}
-        <h2 className="text-xl font-bold">Olá, Natan!</h2>
-        <p>Segunda-feira, 07 de julho.</p>
+        <h2 className="text-xl font-bold">
+          Olá, {session?.user ? session.user.name : "seja bem vindo"}!
+        </h2>
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd", { locale: ptBR })}
+          </span>
+          <span>&nbsp;de&nbsp;</span>
+          <span className="capitalize">
+            {format(new Date(), "MMMM", { locale: ptBR })}
+          </span>
+        </p>
 
         {/* busca */}
         <div className="mt-6">
